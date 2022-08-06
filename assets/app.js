4c69ff059eb7cd508dd773cf63fa3500ae6361fb
Andre Morganti <acmoragnti@gmail.com>
2022-08-06 06:44:57 -0300

    bootstrap

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,18 @@ import './styles/global.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    console.log('opa, entre');
+    $('[data-toggle="popover"]').popover();
+});
