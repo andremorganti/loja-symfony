4c557827face516b99230092984b62845a6dd19e
Andre Morganti <acmoragnti@gmail.com>
2022-08-09 08:19:42 -0300

    adjust crud buttons

---
+++ src/Form/CustomerType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('legal_name')
+            ->add('cnpj')
+            ->add('start_date')
+            ->add('net_revenue')
+            ->add('ebtida')
+            ->add('employees')
+            ->add('info_year')
+            ->add('corporation_structure')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
